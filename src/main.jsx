dcc73e89cf5981f669802fbfea78f39c15c15eab
Omar Barragan <flaky_11@hotmail.com>
2022-09-12 11:19:02 -0500

    probando has router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
 import App from './App';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
