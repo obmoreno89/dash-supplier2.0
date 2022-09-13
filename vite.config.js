08ab4ce347eee4bbd7a1cb674d82eb7b873e438d
Omar Barragan <flaky_11@hotmail.com>
2022-09-13 12:52:48 -0500

    pruebas

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import postcss from './postcss.config.js';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   define: {
     'process.env': process.env,
