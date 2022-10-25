561ddc500adc0bb4f5f2418509373096b1cbf73e
Omar Barragan <flaky_11@hotmail.com>
2022-10-25 10:45:52 -0500

    dashboard version mexico

--- src/pages/PhoneCodeGenerator.jsx
+++ src/pages/PhoneCodeGenerator.jsx
@@ -28,7 +28,6 @@ function PhoneCodeGenerator() {
             {/* Header */}
             <div className='flex-1'>
               <div className='flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8'>
-                {/* Logo */}
                 <Link to='/signin' className='block'>
                   <img src={logohubsupplier} alt='logo hubmine' />
                 </Link>
