4a437e95e0ab78cefc2dde197706eae904f912d9
Omar Barragan <flaky_11@hotmail.com>
2022-10-27 17:31:28 -0500

    api de codigo de telefono funcionando

--- src/pages/PhoneCodeGenerator.jsx
+++ src/pages/PhoneCodeGenerator.jsx
@@ -40,7 +40,6 @@ function PhoneCodeGenerator() {
           <div className='min-h-screen h-full flex flex-col after:flex-1'>
             <div className='flex-1'>
               <div className='flex items-center justify-between h-16 px-4 sm:px-6 lg:px-40'>
-                {/* Logo */}
                 <Link to='/signin' className='block'>
                   <img src={icons.logoSupplier} alt='logo hubmine' />
                 </Link>
