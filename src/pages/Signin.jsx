a937e710ca4230878c154c7e5bd92c72223cf041
Omar Barragan <flaky_11@hotmail.com>
2022-10-27 17:53:15 -0500

    actualizacion de la carpeta dist

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -287,7 +287,6 @@ function Signin() {
             </div>
           </div>
         </div>
-
         <div
           className='hidden md:block absolute top-0 bottom-0 right-0 md:w-1/2'
           aria-hidden='true'>
